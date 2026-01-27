9f0bdce5bbd8b7487a4ea3ad849beb348d855855
zahooruddin-dev <mzkhan886@gmail.com>
2026-01-28 02:25:19 +0500

    base

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`${PORT} is running`);
+});
+
+module.exports = app;
