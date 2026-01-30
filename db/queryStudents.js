a718b53ffaf76378e357d9e51a9fc671e7e8f2c0
zahooruddin-dev <mzkhan886@gmail.com>
2026-01-30 09:35:44 +0500

    typo fix student instead of students

--- db/queryStudents.js
+++ db/queryStudents.js
@@ -1,12 +1,13 @@
 const pool = require('./Pool');
 
 async function getStudentsQuery(id) {
-	await pool.query(`
-		SELECT classes.class_name, student.student_name
+ const {rows} = 	await pool.query(`
+		SELECT classes.class_name, students.student_name
 		FROM classes
-		LEFT JOIN students ON classes.id = student.class_id
+		LEFT JOIN students ON classes.id = students.class_id
 		WHERE classes.id = $1
 		`,[id])
+		return rows
 }
 
 module.exports = {getStudentsQuery}
