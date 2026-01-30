6a12b4d8f69b6ddede005354d1853a1bc96a6e43
zahooruddin-dev <mzkhan886@gmail.com>
2026-01-30 09:51:58 +0500

    BUILT A RELATIONAL DB CREATED NESTED ROUTES SQL JOINS CREATE STUDENT

--- db/queryStudents.js
+++ db/queryStudents.js
@@ -1,19 +1,25 @@
 const pool = require('./Pool');
 
 async function getStudentsQuery(id) {
- const {rows} = 	await pool.query(`
+	const { rows } = await pool.query(
+		`
 		SELECT classes.class_name, students.student_name
 		FROM classes
 		LEFT JOIN students ON classes.id = students.class_id
 		WHERE classes.id = $1
-		`,[id])
-		return rows
+		`,
+		[id],
+	);
+	return rows;
 }
-async function createStudentQuery(name,email,id) {
-	 const {rows} = await pool.query(`
+async function createStudentQuery(name, email, id) {
+	const { rows } = await pool.query(
+		`
 		INSERT INTO students (student_name, email, class_id) VALUES ($1, $2, $3) RETURNING *
-		`, [name,email,id])
-		return rows[0]
+		`,
+		[name, email, id],
+	);
+	return rows[0];
 }
 
-module.exports = {getStudentsQuery,createStudentQuery}
+module.exports = { getStudentsQuery, createStudentQuery };
