97a94a48ea611fe5b827b84cacdc973c9e3ea7d7
zahooruddin-dev <mzkhan886@gmail.com>
2026-01-31 10:03:51 +0500

    query fix stats student instead of students was there it has been fixed and route is working

--- db/queryClassStats.js
+++ db/queryClassStats.js
@@ -3,7 +3,7 @@ const pool = require('./Pool');
 async function getAllClassesStatsQuery() {
 	const { rows } = await pool.query(
 		`
-    SELECT classes.class_name,COUNT(student.id) AS student_count
+    SELECT classes.class_name,COUNT(students.id) AS student_count
     FROM classes
     LEFT JOIN students ON classes.id = students.class_id
     GROUP BY classes.id
