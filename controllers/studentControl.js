aa1fcdc7f6bec62a13df91cfbef1f01e005eb090
zahooruddin-dev <mzkhan886@gmail.com>
2026-01-31 10:25:42 +0500

    search controller

--- controllers/studentControl.js
+++ controllers/studentControl.js
@@ -1,7 +1,14 @@
 const db = require('../db/queryStudents');
 
 async function searchStudents(req, res) {
-
+  const {name} = req.query
+  try{
+    const results = await db.searchStudentsQuery(name)
+    res.json(results).status(200)
+  }
+  catch(err){
+    res.status(500).json({message:'Internal Server Error'})
+  }
 }
 
 module.exports = {
