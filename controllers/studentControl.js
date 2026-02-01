969f5f1f08377b8f84928640c8e6db2575a6a7f8
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-01 07:16:31 +0500

    specific student attending controller

--- controllers/studentControl.js
+++ controllers/studentControl.js
@@ -9,7 +9,17 @@ async function searchStudents(req, res) {
 		res.status(500).json({ message: 'Internal Server Error' });
 	}
 }
+async function specificStudentAttending(req, res) {
+	const { id } = req.params;
+	try {
+		const results = await db.searchStudentsQuery(id);
+		res.json(results).status(200);
+	} catch (err) {
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
+}
 
 module.exports = {
 	searchStudents,
+	specificStudentAttending
 };
