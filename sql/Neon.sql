df2a61fe1d57352d9227deb2f1e04bdfc42c8691
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-01 07:12:13 +0500

    Sql NEON

---
+++ sql/Neon.sql
@@ -0,0 +1,7 @@
+CREATE TABLE enrollments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    student_id UUID REFERENCES students(id) ON DELETE CASCADE,
+    class_id UUID REFERENCES classes(id) ON DELETE CASCADE,
+    enrollment_date DATE DEFAULT CURRENT_DATE,
+    UNIQUE(student_id, class_id) 
+);
