5a83fe814cfb7575afd635fb74fe52ca32f9ad7f
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-01 07:16:49 +0500

    specific student attending route

--- routes/students.js
+++ routes/students.js
@@ -4,6 +4,6 @@ const controller = require('../controllers/studentControl');
 require('dotenv').config;
 
 router.get('/:id', controller.searchStudents);
-router.get('/:id/classes', controller.searchStudents);
+router.get('/:id/classes', controller.specificStudentAttending);
 
 module.exports = router;
