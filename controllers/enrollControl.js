c75af28148055d8d015f410585ca93dbc1fa63cf
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-02 08:58:38 +0500

    validation and status added to get enroll controller

--- controllers/enrollControl.js
+++ controllers/enrollControl.js
@@ -2,6 +2,11 @@ const db = require('../db/queryEnrollment');
 
 async function createEnrollment(req, res) {
 	const { student_id, class_id } = req.body;
+	if (!student_id || !class_id) {
+		return res
+			.status(400)
+			.json({ error: 'student and class ID are required.' });
+	}
 	try {
 		const enrollment = await db.enrollStudent(student_id, class_id);
 		res.status(201).json({
@@ -9,8 +14,9 @@ async function createEnrollment(req, res) {
 			data: enrollment,
 		});
 	} catch (err) {
-		// if student is already enrolled, the UNIQUE constraint should throw an error
+		// if student is already enrolled, the unique constraint should throw an error
 		if (err.code === '23505') {
+			//duplicate key in sql
 			return res
 				.status(400)
 				.json({ message: 'Student is already in this class' });
@@ -28,9 +34,14 @@ async function rooster(req, res) {
 }
 async function getStudentSchedule(req, res) {
 	const { id } = req.params;
+	if (!id) {
+		return res
+			.status(400)
+			.json({ error: 'Id required to be able to get the student shedule' });
+	}
 	try {
 		const schedule = await db.getStudentScheduleQuery(id);
-		res.json(schedule);
+		res.status(201).json(schedule);
 	} catch (err) {
 		res.status(500).json({ error: err.message });
 	}
