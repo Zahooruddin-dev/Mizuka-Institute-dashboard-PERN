9d566228123bee329f68fc50473cb80e756efd96
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-02 16:42:39 +0500

    student update and delete student route

--- routes/classes.js
+++ routes/classes.js
@@ -4,13 +4,13 @@ const controller = require('../controllers/classControl');
 require('dotenv').config;
 
 router.get('/', controller.getClasses);
-
-router.get('/:id', controller.getSpecificClass);
-
-router.put('/:id', controller.editSpecificClass);
-
 router.post('/', controller.createClasses);
 
+
+router.get('/:id', controller.getSpecificClass);
+router.put('/:id', controller.editSpecificClass);
 router.delete('/:id', controller.deleteClass);
+
+
 
 module.exports = router;
--- routes/students.js
+++ routes/students.js
@@ -7,4 +7,7 @@ router.get('/search', controller.searchStudents);
 router.get('/:id', controller.getStudentById); 
 router.get('/:id/classes', controller.getStudentSchedule); 
 
+router.put('/:id', controller.updateStudent);              
+router.delete('/:id', controller.deleteStudent);            
+
 module.exports = router;
