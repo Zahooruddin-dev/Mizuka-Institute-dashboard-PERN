8ed907efcca49c4564ad161d8b62f497a6ebf2c0
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-03 10:26:37 +0500

    popularity query

--- db/queryClassStats.js
+++ db/queryClassStats.js
@@ -11,5 +11,19 @@ async function getAllClassesStatsQuery() {
 	);
 	return rows;
 }
+async function getClassPopularityQuery() {
+	const { rows } = await pool.query(
+		`
+    SELECT 
+          classes.class_name,
+          COUNT(enrollments.students.id)
+           AS student_count
+    FROM classes
+    LEFT JOIN enrollments ON classes.id = enrollments.class_id
+    GROUP BY student_count DESC
+    `,
+	);
+	return rows;
+}
 
-module.exports = { getAllClassesStatsQuery };
+module.exports = { getAllClassesStatsQuery, getClassPopularityQuery };
