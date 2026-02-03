eef6282dc42c40e58db002d4ed12bade59f28a5b
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-03 10:22:41 +0500

    popularity stats route and removeal of express router merge it is no longer needed

--- routes/class-stats.js
+++ routes/class-stats.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 const controller = require('../controllers/statsControl.js');
 require('dotenv').config;
 
 router.get('/', controller.getAllClassesStats);
+router.get('/popularity', controller.getPopularityStats);
 
 module.exports = router;
