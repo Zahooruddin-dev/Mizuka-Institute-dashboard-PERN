a561c47a63bae5765b09ba8949252a8d8a924baf
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-03 10:24:13 +0500

    stats controller status added to get all calsses stats

--- controllers/statsControl.js
+++ controllers/statsControl.js
@@ -3,7 +3,7 @@ const db = require('../db/queryClassStats');
 async function getAllClassesStats(req, res) {
   try {
     const classes = await db.getAllClassesStatsQuery();
-    res.json(classes);
+    res.status(200).json(classes);
   } catch {
     res.status(500).json({ message: 'Internal server error' });
   }
