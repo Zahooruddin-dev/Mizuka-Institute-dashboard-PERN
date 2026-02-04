ad0d320a34746a139214c00ce01edeb601ca5673
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-04 10:23:04 +0500

    WE HAVE FINISHED THE API WORK THE API IS FUNCTIONAL WITH PAGINTATION WORKING AS INTENDED WE HAVE REMOVED GET ALL STUDENTS TO PAGINTATION ONE WE HAVE ADDED STATUS 200 WHERE REQUIRED NOW READY FOR FRONTEND USE

--- routes/students.js
+++ routes/students.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/studentControl');
-const enrollController = require('../controllers/enrollControl')
-const statsController = require('../controllers/statsControl')
+const enrollController = require('../controllers/enrollControl');
+const statsController = require('../controllers/statsControl');
 require('dotenv').config;
 
-router.get('/', controller.getAllStudentsPagination)
+router.get('/', controller.getAllStudentsPagination);
 router.get('/search', controller.searchStudents);
 router.get('/workload', statsController.getAllStudentsStats);
 
-router.get('/:id', controller.getStudents); 
-router.get('/:id/classes', enrollController.getStudentSchedule); 
+router.get('/:id', controller.getStudents);
+router.get('/:id/classes', enrollController.getStudentSchedule);
 router.get('/:id/students', enrollController.rooster);
 
-router.put('/:id', controller.updateStudent);              
-router.delete('/:id', controller.deleteStudent);            
+router.put('/:id', controller.updateStudent);
+router.delete('/:id', controller.deleteStudent);
 
 module.exports = router;
