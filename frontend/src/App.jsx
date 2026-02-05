992e94c3b289f4394745daf906a1f7cebc916134
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-05 15:15:03 +0500

    data being fetched from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,22 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
- useEffect(()=>{
-  axios.get('http://http://localhost:3000/api/students')
-  .then(res=>console.log('Connection succeful students found', res.data)
-  .then(err => console.error("Connection failed", err);
-  )
-  )
- })
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		axios
+			.get('http://localhost:3000/api/students')
+			.then((res) =>
+				console.log('Connection succeful students found', res.data),
+			)
+			.catch((err) => console.error('Connection failed', err));
+	}, []);
+	return (
+		<div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+			<h1>Institutes Dashboard</h1>
+			<p>check the console (F12) to see if backend worked or not </p>
+		</div>
+	);
 }
 
-export default App
+export default App;
