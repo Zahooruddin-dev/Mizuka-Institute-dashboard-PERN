3aa03247a9b8d7e2207317df44a6a1cf20493362
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-05 15:17:41 +0500

    student data states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,26 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+	const [students, setStudents] = useState([]);
+	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		axios
 			.get('http://localhost:3000/api/students')
-			.then((res) =>
-				console.log('Connection succeful students found', res.data),
+			.then(
+				(res) => console.log('Connection succeful students found', res.data),
+				setStudents(res.data.students),
+				setLoading(false),
 			)
-			.catch((err) => console.error('Connection failed', err));
+			.catch(
+				(err) => console.error('Connection failed', err),
+				setLoading(false),
+			);
 	}, []);
 	return (
 		<div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
 			<h1>Institutes Dashboard</h1>
 			<p>check the console (F12) to see if backend worked or not </p>
+			<li></li>
 		</div>
 	);
 }
