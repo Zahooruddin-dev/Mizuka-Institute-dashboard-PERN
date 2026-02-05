56cb81f40f87c949248eb0e0a578735b48f62d96
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-05 15:23:17 +0500

    students fetching and displaying of the data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,32 +8,38 @@ function App() {
 	useEffect(() => {
 		axios
 			.get('http://localhost:3000/api/students')
-			.then(
-				(res) => console.log('Connection succeful students found', res.data),
-				setStudents(res.data.students),
-				setLoading(false),
-			)
-			.catch(
-				(err) => console.error('Connection failed', err),
-				setLoading(false),
-			);
+			.then((res) => {
+				(setStudents(res.data.students), setLoading(false));
+			})
+			.catch((err) => {
+				(console.error('Connection failed', err), setLoading(false));
+			});
 	}, []);
 	if (loading) return <h1>Loading Students...</h1>;
 	return (
 		<div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-			<table border="1" cellPadding="10" style={{ width: '100%', textAlign: 'left' }}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-        
-        </tbody>
-      </table>
-      {/* If array is empty, shows a message */}
-      {students.length === 0 && <p>No students found.</p>}
+			<table
+				border='1'
+				cellPadding='10'
+				style={{ width: '100%', textAlign: 'left' }}
+			>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					{students.map((student) => {
+						<tr key={student.id}>
+							<td>{student.name}</td>
+							<td>{student.email}</td>
+						</tr>;
+					})}
+				</tbody>
+			</table>
+			{/* If array is empty, shows a message */}
+			{students.length === 0 && <p>No students found.</p>}
 		</div>
 	);
 }
