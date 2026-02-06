72088fc2399e8520081dfde8b68199c5f7d7782f
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-06 10:27:11 +0500

    post student route added

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -6,6 +6,8 @@ const statsController = require('../controllers/statsControl');
 require('dotenv').config;
 
 router.get('/', controller.getAllStudentsPagination);
+router.post('/', controller.addStudent);
+
 router.get('/search', controller.searchStudents);
 router.get('/workload', statsController.getAllStudentsStats);
 
@@ -14,6 +16,7 @@ router.get('/:id/classes', enrollController.getStudentSchedule);
 router.get('/:id/students', enrollController.rooster);
 
 router.put('/:id', controller.updateStudent);
+
 router.delete('/:id', controller.deleteStudent);
 
 module.exports = router;
