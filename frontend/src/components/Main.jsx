f484a05a57b874bf4cf03a5d3023a4b52ccf035b
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-08 15:02:49 +0500

    styled search logo and div

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,21 +1,26 @@
-import { Edit, Trash2 } from 'lucide-react';
+import { Edit, Search, Trash2 } from 'lucide-react';
 import StudentTable from './StudentTable';
+
 export default function MainComponent({
 	handleEdit,
 	handleDelete,
 	searchTerm,
 	students,
-  setSearchTerm
+	setSearchTerm,
 }) {
 	return (
 		<>
-			<input
-				type='text'
-				placeholder='Search student by name'
-				value={searchTerm}
-				onChange={(e) => setSearchTerm(e.target.value)}
-				style={{ marginBottom: '20px', padding: '8px', width: '300px' }}
-			/>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<input
+					type='text'
+					placeholder='Search student by name'
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					style={{ marginBottom: '20px', padding: '8px', width: '300px' }}
+				/>
+				<Search size={25} color='wheat' style={{ padding: '8px' }} />
+			</div>
+
 			<StudentTable
 				handleEdit={handleEdit}
 				students={students}
