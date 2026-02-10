060000bcebb105ada1bf7980d185d57af5310ee8
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-10 07:43:46 +0500

    additioanl routes added

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -3,20 +3,25 @@ const router = express.Router();
 const controller = require('../controllers/studentControl');
 const enrollController = require('../controllers/enrollControl');
 const statsController = require('../controllers/statsControl');
-require('dotenv').config;
+require('dotenv').config();
 
 router.get('/', controller.getAllStudentsPagination);
 router.post('/', controller.addStudent);
 
 router.get('/search', controller.searchStudents);
-router.get('/workload', statsController.getAllStudentsStats);
+router.get('/stats', statsController.getAllStudentsStats);
+router.get('/count', controller.getTotalStudentsCount);
+router.get('/recent', controller.getRecentStudents);
 
-router.get('/:id', controller.getStudents);
+router.get('/:id', controller.getStudentById);
 router.get('/:id/classes', enrollController.getStudentSchedule);
-router.get('/:id/students', enrollController.rooster);
+router.get('/:id/enrollment', enrollController.getStudentEnrollments);
+router.get('/:id/grades', controller.getStudentGrades);
 
 router.put('/:id', controller.updateStudent);
+router.patch('/:id/email', controller.updateStudentEmail);
 
 router.delete('/:id', controller.deleteStudent);
+router.delete('/bulk', controller.bulkDeleteStudents);
 
 module.exports = router;
