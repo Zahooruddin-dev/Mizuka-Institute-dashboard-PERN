565d3ff97c2f21eea5973c72e07a58eb28dbec8f
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-11 04:22:44 +0500

    Auth route added to app

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const classRoutes = require('./routes/classes');
 const classStatsRoute = require('./routes/class-stats');
 const studentsRoute = require('./routes/students');
 const enrollRoutes = require('./routes/enrollRoutes');
+const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -15,6 +16,7 @@ app.use('/api/class', classRoutes);
 app.use('/api/class-stats', classStatsRoute);
 app.use('/api/students', studentsRoute);
 app.use('/api/enroll', enrollRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
 	console.log(`${PORT} is running`);
