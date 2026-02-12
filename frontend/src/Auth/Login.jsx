e18d520efe27d72b0f6bed624af83751d7bda2b4
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-12 21:44:57 +0500

    Added handle change and and handle submit to deal with form data state as well as perform submission so that use is logged in and token is stored in local storage the token (Note not storing in session token but local storage)

--- frontend/src/Auth/Login.jsx
+++ frontend/src/Auth/Login.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { useState } from 'react';
+import { loginUser } from '../api/authApi';
+import { useNavigate } from 'react-router';
 
 export default function Login() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-
+	const navigate = useNavigate();
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+	});
+	const [error, setError] = useState('');
+	const handleChange = (e) => {
+		setFormData((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setError('');
+		try {
+      const response = await loginUser(formData)
+      const token = response.data.token
+      localStorage.setItem('token',token)
+      navigate('/')
+		} catch (error) {
+			setError(error.response?.data?.message || 'Login Failed');
+		}
+	};
 	return (
-		<>
-			<div style={{ backgroundColor: 'grey', padding: '50px' }}>
-				<input type='email' />
-				<input type='password' />
-			</div>
-		</>
+		<div className='auth-container'>
+			<form className='auth-form'></form>
+		</div>
 	);
 }
