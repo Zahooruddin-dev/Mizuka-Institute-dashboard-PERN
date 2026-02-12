e896dd98f5cde68f3a94cc193d281a4c73156e3f
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-12 22:29:09 +0500

    One shared Axios instance is used everywhere.

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,13 +1,4 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:3000/api/students';
-
-const api = axios.create({
-	baseURL: API_BASE_URL,
-	headers: {
-		'Content-Type': 'application/json',
-	},
-});
+import api from './axiosConfig';
 
 export const getStudents = (params) => {
 	return api.get('/', { params });
