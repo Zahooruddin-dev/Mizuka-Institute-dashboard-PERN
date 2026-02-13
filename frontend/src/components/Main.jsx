63e8fd6ff097c4994bbef5c3de39139e61259360
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-14 02:26:28 +0500

    main styling file

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,6 +1,6 @@
 import { Search, ArrowUpAZ, ArrowDownZA } from 'lucide-react';
 import StudentTable from './StudentTable';
-import './MainComponent.css';
+import './main.css';
 
 export default function MainComponent({
 	handleEdit,
