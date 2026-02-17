b1b0f1fad409c77748f1cebd9c9318df4886de5f
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-18 00:33:05 +0500

    post enrollment, get class enrolled rooster and get student enrolled shedule routes via axios in api js added

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,34 +1,43 @@
 import api from './axiosConfig';
 
 export const getStudents = (params) => {
-  return api.get('/api/students', { params });
+	return api.get('/api/students', { params });
 };
 
 export const createStudent = (data) => {
-  return api.post('/api/students', data);
+	return api.post('/api/students', data);
 };
 
 export const updateStudent = (id, data) => {
-  return api.put(`/api/students/${id}`, data);
+	return api.put(`/api/students/${id}`, data);
 };
 
 export const deleteStudent = (id) => {
-  return api.delete(`/api/students/${id}`);
+	return api.delete(`/api/students/${id}`);
 };
 export const getStudentById = (id) => {
-  return api.get(`/api/students/${id}`);
+	return api.get(`/api/students/${id}`);
 };
 
 export const getStudentClasses = (id) => {
-  return api.get(`/${id}/classes`);
+	return api.get(`/${id}/classes`);
 };
 
-export const createClass = (data) =>{
-  return api.post('/api/class',data)
-}
-export const getClasses = (params) =>{
-  return api.get('/api/class',{params})
-}
+export const createClass = (data) => {
+	return api.post('/api/class', data);
+};
+export const getClasses = (params) => {
+	return api.get('/api/class', { params });
+};
 export const getClassById = (id) => {
-  return api.get(`api/class/${id}`);
+	return api.get(`api/class/${id}`);
+};
+export const postEnrollement = (data) => {
+	return api.get(`api/enroll`, data);
+};
+export const getClassEnrolledRooster = (id) => {
+	return api.get(`api/enroll/class/${id}`);
+};
+export const getStudentEnrolledShedule = (id) => {
+	return api.get(`api/student/class/${id}`);
 };
