014c4cf83db78f6ea3654c03f33abc338578a845
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-18 04:20:16 +0500

    Updated query edit class since we have updated the table classes it was required to keep it funciatoinal

--- backend/db/queryClasses.js
+++ backend/db/queryClasses.js
@@ -16,15 +16,17 @@ async function CreateNewClassQuery(class_name, time_in_pakistan, teacher_id) {
   return rows[0]; 
 }
 async function queryEditClassQuery(id, data) {
-	const { class_name, time_in_pakistan } = data;
-	await pool.query(
-		`
-		UPDATE classes
-		SET class_name = $1, time_in_pakistan = $2
-		WHERE id = $3
+  const { class_name, time_in_pakistan } = data;
+  const { rows } = await pool.query(
+    `
+    UPDATE classes
+    SET class_name = $1, time_in_pakistan = $2
+    WHERE id = $3
+    RETURNING *
     `,
-		[class_name, time_in_pakistan, id],
-	);
+    [class_name, time_in_pakistan, id],
+  );
+  return rows[0];
 }
 async function getClassByIdQuery(id) {
 	const { rows } = await pool.query('SELECT * FROM classes WHERE id = $1', [
