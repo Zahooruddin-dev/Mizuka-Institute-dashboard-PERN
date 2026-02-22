0212aa4b608b4a70e1241452094905c5a00c2be2
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-23 03:25:41 +0500

    Cloudinary middleware reaplced the uplaod middlware in the same fiel and the cldoluarinary mdidlware file deleted

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,21 +1,29 @@
-const { error } = require('console');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
-const path = require('path');
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'uploads/');
-	},
-	filename: (req, file, cb) => {
-		cb(null, `avatar-${Date.now()}${path.extname(file.originalname)}`);
-	},
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'mizuka_uploads', 
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+  },
+});
+
 const fileFilter = (req, file, cb) => {
-	if (file.mimetype.startsWith('image/')) {
-		cb(null, true);
-	} else {
-		cb(new error('Not an image! Please upload an image.'), false);
-	}
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Not an image! Please upload an image.'), false);
+  }
 };
+
 const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
