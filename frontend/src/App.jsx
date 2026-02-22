1ae1b60c3bf2fa58dd34e5f7c7b4f6bb0d6319ed
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-22 10:26:35 +0500

    User fetching done via useAuth in the parent app componetn providing data to all the compoents

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,49 +8,50 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Layout from './pages/Layout';
 import ProtectedRoute from './utils/ProtectedRoute';
-import { getUserFromToken } from './utils/auth';
+import { AuthProvider, useAuth } from './utils/AuthContext';
 import ForgotPassword from './pages/Auth/Forgotpassword';
 import './App.css';
 
 const AuthRedirect = ({ children }) => {
-	const user = getUserFromToken();
-	if (user) {
-		return <Navigate to='/' replace />;
-	}
+	const { user } = useAuth();
+	if (user) return <Navigate to='/' replace />;
 	return children;
 };
+
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<ProtectedRoute>
-							<Layout />
-						</ProtectedRoute>
-					}
-				/>
-				<Route
-					path='/login'
-					element={
-						<AuthRedirect>
-							<Login />
-						</AuthRedirect>
-					}
-				/>
-				<Route
-					path='/register'
-					element={
-						<AuthRedirect>
-							<Register />
-						</AuthRedirect>
-					}
-				/>
-				<Route path='/forgot-password' element={<ForgotPassword />} />
-				<Route path='*' element={<Navigate to='/' replace />} />
-			</Routes>
-		</Router>
+		<AuthProvider>
+			<Router>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<ProtectedRoute>
+								<Layout />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/login'
+						element={
+							<AuthRedirect>
+								<Login />
+							</AuthRedirect>
+						}
+					/>
+					<Route
+						path='/register'
+						element={
+							<AuthRedirect>
+								<Register />
+							</AuthRedirect>
+						}
+					/>
+					<Route path='/forgot-password' element={<ForgotPassword />} />
+					<Route path='*' element={<Navigate to='/' replace />} />
+				</Routes>
+			</Router>
+		</AuthProvider>
 	);
 }
 
