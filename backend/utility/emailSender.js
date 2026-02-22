d5ee3faa8cbdcdf58bd2d1ef36f80982182527d0
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-23 04:26:58 +0500

    Reset code sending successfully implmeneted now all the codes are being emailed to the user registered email address

--- backend/utility/emailSender.js
+++ backend/utility/emailSender.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER, // Your Gmail
-    pass: process.env.EMAIL_PASS, // The 16-character App Password
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
   },
 });
 
