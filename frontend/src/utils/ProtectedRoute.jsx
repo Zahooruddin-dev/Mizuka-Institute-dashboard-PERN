d6991718b6eaa9a2647c224015fc4d1d3373442b
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-22 10:25:23 +0500

    useAuth implemented in protected route file

--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -1,12 +1,9 @@
 import { Navigate } from 'react-router-dom';
-import { getUserFromToken } from './auth';
+import { useAuth } from './AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-	const user = getUserFromToken();
-
-	if (!user) {
-		return <Navigate to="/login" replace />;
-	}
+	const { user } = useAuth();
+	if (!user) return <Navigate to='/login' replace />;
 	return children;
 };
 
