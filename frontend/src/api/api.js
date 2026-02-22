e83852a1afa2826b1d10aed4af60309e1ea07c16
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-22 10:06:10 +0500

    All student route imports in api js in frontend deleted

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,20 +1,5 @@
 import api from './axiosConfig';
 
-export const getStudents = (params) => {
-	return api.get('/api/students', { params });
-};
-export const createStudent = (data) => {
-	return api.post('/api/students', data);
-};
-export const updateStudent = (id, data) => {
-	return api.put(`/api/students/${id}`, data);
-};
-export const deleteStudent = (id) => {
-	return api.delete(`/api/students/${id}`);
-};
-export const getStudentById = (id) => {
-	return api.get(`/api/students/${id}`);
-};
 export const getStudentClasses = (id) => {
 	return api.get(`/${id}/classes`);
 };
