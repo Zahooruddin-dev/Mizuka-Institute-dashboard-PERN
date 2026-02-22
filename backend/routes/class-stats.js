713806d268d14719a39d9d965185344587f2fc8b
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-22 10:49:29 +0500

    classStatsRoute route file removed

--- backend/routes/class-stats.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/statsControl.js');
-require('dotenv').config;
-
-router.get('/', controller.getAllClassesStats);
-router.get('/popularity', controller.getPopularityStats);
-
-module.exports = router;
