1cb9e3e5427a129c5f2d2f6923d38ebf189fe63f
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-23 05:23:47 +0500

    Frotend now taking in the baseURL from enviorment varaible

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,17 +1,18 @@
-import axios from "axios";
-const api = axios.create ({
-  baseURL:'http://localhost:3000',
-  headers:{
-"Content-Type": "application/json"
-  }
-})
-api.interceptors.request.use((config)=>{
-  const token = localStorage.getItem('token')
-  if(token){
-    config.headers.Authorization=`Bearer ${token}`
-  }
-  return config
-},
-  (error) => Promise.reject(error)
-)
-export default api
+import axios from 'axios';
+const api = axios.create({
+	baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+});
+api.interceptors.request.use(
+	(config) => {
+		const token = localStorage.getItem('token');
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => Promise.reject(error),
+);
+export default api;
