fa12a91ee21a7240c922d7c6801c1a1bfc716409
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-25 06:25:20 +0500

    Using get user By Email instead of login query in login for chekcing weather user exsits or not

--- backend/controllers/authControl.js
+++ backend/controllers/authControl.js
@@ -6,7 +6,7 @@ const db = require('../db/queryAuth');
 async function login(req, res) {
 	const { email, password } = req.body;
 	try {
-		const user = await db.loginQuery(email);
+		const user = await db.getUserByEmail(email);
 		if (!user)
 			return res.status(401).json({ message: 'Invalid Email or Password' });
 
